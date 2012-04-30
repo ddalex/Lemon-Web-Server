cfa097ceab8adf6eb6b4e629abb7f7e2ddad2d14
Alex Damian <ddalex@gmail.com>
2012-04-30 15:25:52 +0300

    Finish test implementation for LockingQueue.
    Configurable in thread numbers and size, this test
    completes the LockingQueue functional testing.

--- tests/t1_queue.cpp
+++ tests/t1_queue.cpp
@@ -8,20 +8,28 @@
 #include "Exception.h"
 using namespace lemon;
 
+#define NUM_THREADS	4000
+#define NUM_TESTS	1000
 
 LockingQueue<void> queue;
 
+unsigned short int 	marker[NUM_TESTS*NUM_THREADS];
+pthread_mutex_t 	marker_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 void* creator(void *ptr)
 {
+	int id = (long unsigned int) ptr;
 	int i;
 	srand(0);
-	for (i = 0; i < 50; i++ )
+	for (i = 0; i < NUM_TESTS; i++ )
 	{
 		LockedElement<void> *le = new LockedElement<void>;
-		le->SetPayload((void *)i);
+		le->SetPayload((void *)(id*NUM_TESTS+i));
 		queue.Push(le);
-		printf("Pushed %lx! %x\n", (long unsigned int) ptr, i);
-		usleep(1);
+
+		pthread_mutex_lock(&marker_mutex);
+		marker[id*NUM_TESTS+i] ++;
+		pthread_mutex_unlock(&marker_mutex);
 	}
 	return NULL;
 }
@@ -31,25 +39,49 @@ void* destroyer(void *ptr)
 	int i = 0;
 	do {
 		    LockedElement<void> *le = queue.Pop();
-			printf("Popped %lx! %lx ", (long unsigned int) ptr, (long unsigned int)le->GetPayload());
-			printf("size: %lu\n", queue.GetSize());
-			delete le;
 			i++;
-			usleep(1);
-	} while (i < 50);
+			pthread_mutex_lock(&marker_mutex);
+			marker[(long unsigned int)le->GetPayload()]--;
+			pthread_mutex_unlock(&marker_mutex);
+
+			delete le;
+	} while (i < NUM_TESTS);
+}
+
+int test_proc()
+{
+	int id = 0;
+	int retval = 0;
+	pthread_t producer[NUM_THREADS], consumer[NUM_THREADS];
+
+
+	for (id = 0; id < NUM_TESTS * NUM_THREADS; id++)
+			marker[id] = 0;
+
+	for (id = 0; id < NUM_THREADS; id++) {
+		pthread_create(&producer[id], NULL, creator, (void *)id);
+		pthread_create(&consumer[id], NULL, destroyer, (void *)id);
+	}
+
+	for (id = 0; id < NUM_THREADS; id++) {
+		pthread_join(producer[id], NULL);
+	    pthread_join(consumer[id], NULL);
+	}
+
+	if (queue.GetSize() != 0)
+		EXCEPTION("Queue not consumed");
+
+	for (id = 0; id < NUM_TESTS * NUM_THREADS; id++)
+		if (marker[id] != 0) {
+			fprintf(stderr, "test failed: element id %d val %d\n", id, marker[id]);
+			retval ++;
+		}
+
+	return retval;
 }
 
 int main(int argc, char**argv)
 {
-	pthread_t producer[2], consumer[2];
-	pthread_create(&producer[0], NULL, creator, (void *)1);
-	pthread_create(&producer[1], NULL, creator, (void *)2);
-    pthread_create(&consumer[0], NULL, destroyer, (void *)1);
-    pthread_create(&consumer[1], NULL, destroyer, (void *)2);
-    pthread_join(producer[0], NULL);
-    pthread_join(producer[1], NULL);
-    pthread_join(consumer[0], NULL);
-    pthread_join(consumer[1], NULL);
-	return 0;
+	return test_proc();
 }
 
