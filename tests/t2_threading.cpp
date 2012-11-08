999b5fa3af84965ebea14fa016992b588c12399d
Alex DAMIAN <ddalex@gmail.com>
2012-11-08 12:55:12 +0200

    Change testing procedure for the threading test
    These changes make tests easier to use.
    
    * Test to be run can be selected as command line parameter
    * There is just a single test ran at once
    * Fix in the modules needed to be linked in.
    
    Signed-off-by: Alex DAMIAN <ddalex@gmail.com>

--- tests/t2_threading.cpp
+++ tests/t2_threading.cpp
@@ -1,25 +1,33 @@
 #define MAX_THREADS 20
 
+#include <stdlib.h>
 #include <stdio.h>
 #include "ThreadingTest.h"
 
 int main(int argv, char **argc) {
 	ThreadingTest t;
-	ThreadingTest tx[MAX_THREADS];
-	int tests = 3;
+	ThreadingTest tx[MAX_THREADS] = { 1, 2 } ;
+	int tests = 1;
 	int i = 0;
+
+	if (argv >= 2)
+		tests = atoi(argc[1]);
+
+	tests = tests < 1 ? 1 : tests > 3 ? 3 : tests;
+
+	printf("Running test %d\n", tests);
 
 	switch (tests) {
 	case 1:
 		t.Start();
 		sleep(1);
 		t.Stop();
-		printf("----------\n");
+		break;
 	case 2:
 		t.Start();
 		sleep(1);
 		lemon::ThreadingManager::getThreadingManager()->signalThreadAllStop();
-		printf("----------\n");
+		break;
 	case 3:
 		for (i = 0; i < MAX_THREADS; i++)
 		{
@@ -27,5 +35,6 @@ int main(int argv, char **argc) {
 		}
 		sleep(1);
 		lemon::ThreadingManager::getThreadingManager()->signalThreadAllStop();
+		break;
 	}
 }
