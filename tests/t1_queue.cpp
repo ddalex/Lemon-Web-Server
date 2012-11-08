6620a973a983607c65e2059951a1a0d2075e8521
Alex DAMIAN <ddalex@gmail.com>
2012-11-08 12:48:57 +0200

    Fix queueing test
    Several fixes to the queueing test leads to more correct
    tests of the queueing structures
    
    * Correctly verify creation and distruction of threads
    * Adds independent time delays in producer/consumer so
    that various interlocking scenarios may be tested
    * Reduce number of threads to sane defaults
    
    Signed-off-by: Alex DAMIAN <ddalex@gmail.com>

--- tests/t1_queue.cpp
+++ tests/t1_queue.cpp
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <iostream>
 
 #include "LockingQueue.h"
 #include "LockedElement.h"
 #include "Exception.h"
 using namespace lemon;
 
-#define NUM_THREADS	4000
+#define NUM_THREADS	100
 #define NUM_TESTS	1000
 
 LockingQueue<void> queue;
@@ -25,7 +26,7 @@ void* creator(void *ptr)
 	{
 		LockedElement<void> *le = new LockedElement<void>((void *)(id*NUM_TESTS+i));
 		queue.Push(le);
-
+		usleep(27);
 		pthread_mutex_lock(&marker_mutex);
 		marker[id*NUM_TESTS+i] ++;
 		pthread_mutex_unlock(&marker_mutex);
@@ -39,12 +40,14 @@ void* destroyer(void *ptr)
 	do {
 		    LockedElement<void> *le = queue.Pop();
 			i++;
+			usleep(47);
 			pthread_mutex_lock(&marker_mutex);
 			marker[(long unsigned int)le->GetPayload()]--;
 			pthread_mutex_unlock(&marker_mutex);
 
 			delete le;
 	} while (i < NUM_TESTS);
+	return NULL;
 }
 
 int test_proc()
@@ -57,18 +60,48 @@ int test_proc()
 	for (id = 0; id < NUM_TESTS * NUM_THREADS; id++)
 			marker[id] = 0;
 
+	int max_producer_thread_no, max_consumer_thread_no;
 	for (id = 0; id < NUM_THREADS; id++) {
-		pthread_create(&producer[id], NULL, creator, (void *)id);
-		pthread_create(&consumer[id], NULL, destroyer, (void *)id);
+		int error;
+
+		if (0 != (error = pthread_create(&producer[id], NULL, creator, (void *)id))) {
+			std::cerr << "failed creating producer thread " << id << ": " << strerror(error) << std::endl;
+			memset(&producer[id], 0, sizeof(pthread_t));
+		}
+		else
+			max_producer_thread_no = id;
+		if (0 != (error = pthread_create(&consumer[id], NULL, destroyer, (void *)id))) {
+			std::cerr << "failed creating consumer thread " << id << ": " << strerror(error) << std::endl;
+			memset(&consumer[id], 0, sizeof(pthread_t));
+		}
+		else
+			max_consumer_thread_no = id;
 	}
 
-	for (id = 0; id < NUM_THREADS; id++) {
-		pthread_join(producer[id], NULL);
-	    pthread_join(consumer[id], NULL);
+	std::cout << "Created " << max_producer_thread_no << " producers and " << max_consumer_thread_no << " consumers" << std::endl;
+
+	for (id = 0; id < max_producer_thread_no; id++) {
+		int error;
+		pthread_t nullthread;
+		memset(&nullthread, 0, sizeof(pthread_t));
+		if ((memcmp(&nullthread, &producer[id], sizeof(pthread_t)) != 0) && 0 != (error = pthread_join(producer[id], NULL))) {
+			std::cerr << "failed joining producer thread " << id << ": " << strerror(error) << std::endl;
+		}
 	}
 
-	if (queue.GetSize() != 0)
-		EXCEPTION("Queue not consumed");
+	std::cout << "Done with producers, " << queue.GetSize() << " elements remaining " << std::endl;
+	while (queue.GetSize() != 0) usleep(10);
+
+
+	for (id = 0; id < max_consumer_thread_no; id++) {
+		int error;
+		pthread_t nullthread;
+		memset(&nullthread, 0, sizeof(pthread_t));
+		if ((memcmp(&nullthread, &consumer[id], sizeof(pthread_t)) != 0) && 0 != (error = pthread_join(consumer[id], NULL))) {
+	    	std::cerr << "failed joining consumer thread " << id << ": " << strerror(error) << std::endl;
+		}
+	}
+
 
 	for (id = 0; id < NUM_TESTS * NUM_THREADS; id++)
 		if (marker[id] != 0) {
